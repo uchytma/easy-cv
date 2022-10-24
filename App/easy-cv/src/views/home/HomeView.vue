92a5cb35b321e1d97595632b0251b8c156fb8185
Matěj Uchytil <uchytma@gmail.com>
2022-10-24 18:02:49 +0200

    Landing page styles

--- App/easy-cv/src/views/home/HomeView.vue
+++ App/easy-cv/src/views/home/HomeView.vue
@@ -2,7 +2,9 @@
   <main>
     <h1>Easy CV</h1>
     <p>Vytvořte si moderní životopis během pár minut.</p>
-    <button @click="() => router.push('/app')">Začít tvořit</button>
+    <div>
+      <button @click="() => router.push('/app')">Začít tvořit</button>
+    </div>
   </main>
 </template>
 
@@ -11,3 +13,21 @@ import { useRouter } from "vue-router";
 
 const router = useRouter();
 </script>
+
+<style lang="css" scoped>
+h1 {
+  margin: 0;
+}
+
+main {
+  padding: var(--base-gap);
+  display: flex;
+  flex-direction: column;
+  gap: var(--base-gap);
+}
+
+button {
+  padding: var(--base-gap);
+  cursor: pointer;
+}
+</style>
